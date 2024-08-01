062e8b45ebb372b9a10361c411bce9908521f437
udodovr <udodovr03@gmail.com>
2024-08-01 06:14:37 +0300

    Sending real id instead of index & getting list on button pushed

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import * as Dialog from '@radix-ui/react-dialog';
+
+import styles from './Modal.module.sass';
+
+const Modal = ({
+    setRejectedBulletins,
+    setApprovedBulletins,
+    setEscalatedBulletins,
+    selectedBriefId,
+}) => {
+    const [reason, setReason] = useState('');
+
+    const handleButtonClick = () => {
+        setRejectedBulletins(
+            (previous) => new Set(previous.add({ id: selectedBriefId - 1, reason: reason }))
+        );
+        setApprovedBulletins((previous) => {
+            previous.delete(selectedBriefId);
+            return new Set(previous);
+        });
+        setEscalatedBulletins((previous) => {
+            previous.delete(selectedBriefId);
+            return new Set(previous);
+        });
+        setReason('');
+    };
+
+    return (
+        <Dialog.Portal>
+            <Dialog.Overlay className={styles.dialogOverlay} />
+            <Dialog.Content className={styles.dialogContent}>
+                <Dialog.Title className={styles.dialogTitle}>Причина отклонения</Dialog.Title>
+
+                <textarea
+                    cols="29"
+                    rows="5"
+                    minLength="10"
+                    maxLength="500"
+                    value={reason}
+                    onChange={(e) => setReason(e.target.value)}
+                ></textarea>
+
+                <div>
+                    <Dialog.Close asChild>
+                        <button className={styles.button} onClick={() => handleButtonClick()}>
+                            Сохранить
+                        </button>
+                    </Dialog.Close>
+                </div>
+            </Dialog.Content>
+        </Dialog.Portal>
+    );
+};
+
+export default Modal;
